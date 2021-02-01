99d41463e12d4e1a9c320f36521a99a4652c9612
Ian Quinn <44006950+IanQuinn22@users.noreply.github.com>
2021-02-01 13:19:41 -0500

    update server for multiple users

--- blephotoidentifier_server.py
+++ blephotoidentifier_server.py
@@ -9,8 +9,28 @@ matchList = []
 encodingList = []
 # Add names and photo locations of known encodings here
 personDict = {
-    "Barack Obama": "obama.jpg"
+    "Barack Obama": "obama.jpg",
+    "Joe Biden": "biden.jpg"
 }
+obama_image_file = personDict.get("Barack Obama")
+biden_image_file = personDict.get("Joe Biden")
+if obama_image_file != None:
+    known_image = face_recognition.load_image_file(obama_image_file)
+    face_locations = face_recognition.face_locations(known_image)
+    print(face_locations)
+    print(face_recognition.face_encodings(known_image)[0])
+    encodingList.append([face_recognition.face_encodings(known_image)[0],"Barack Obama"])
+else:
+    encodingList.append([None,None])
+
+if biden_image_file != None:
+    known_image = face_recognition.load_image_file(biden_image_file)
+    face_locations = face_recognition.face_locations(known_image)
+    print(face_locations)
+    print(face_recognition.face_encodings(known_image)[0])
+    encodingList.append([face_recognition.face_encodings(known_image)[0],"Joe Biden"])
+else:
+    encodingList.append([None,None])
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
@@ -25,23 +45,29 @@ def handle_request():
     print(cv2.imwrite(imagefile.filename,rotatedImage))
     uploaded_image = face_recognition.load_image_file(imagefile.filename)
     image_encodings = face_recognition.face_encodings(uploaded_image)
-    found = False
+    print(len(image_encodings))
     for index in range(len(image_encodings)):
+        found = False
         unknown_encoding = image_encodings[index]
         for index2 in range(len(encodingList)):
             if encodingList[index2][0].all() != None:
-                if face_recognition.compare_faces([encodingList[index2][0]],unknown_encoding):
+                match = face_recognition.compare_faces([encodingList[index2][0]],unknown_encoding)
+                if match[0]:
                     matchList.append(encodingList[index2][1])
+                    print(encodingList[index2][1])
                     found = True
                     break
         if not found:
-            matchList.append([None])
+            matchList.append(None)
     orderedList = []
     for k in range(len(matchList)):
-        if ((matchList[k][0] not in orderedList) | (matchList[k][0]==None)):
-            orderedList.append(matchList[k][0])
+        if ((matchList[k] not in orderedList) | (matchList[k]==None)):
+            if (matchList[k] == None):
+                orderedList.append("None")
+            else:
+                orderedList.append(matchList[k])
     orderNames = "*".join(orderedList)
-
+    print(orderNames)
     return orderNames
 
 @app.route('/names', methods=['GET', 'POST'])
@@ -49,14 +75,7 @@ def get_names():
     names = flask.request.data
     namesStr = names.decode('utf-8')
     nameList = namesStr.split('*')
-    for i in range(len(nameList)):
-        image_file = personDict.get(nameList[i])
-        if image_file != None:
-            known_image = face_recognition.load_image_file(image_file)
-            face_locations = face_recognition.face_locations(known_image)
-            encodingList.append([face_recognition.face_encodings(known_image)[0],[nameList[i]]])
-        else:
-            encodingList.append([None,None])
+    print(nameList[0] + "h" + nameList[1])
     return "Name(s) Received"
 
 app.run(host="0.0.0.0", port=5000, debug=True)
